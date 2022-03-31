fc2f734d7fdcc5ab61cc586fd88daeb24d8ffb91
Michalol94 <michal.olech94@gmail.com>
2022-03-31 06:57:44 +0200

    Firebase installed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'todo-app-f2a04',
+    appId: '1:511326328594:web:6e2ebe09e7d3a7a61c05cb',
+    storageBucket: 'todo-app-f2a04.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyD8q-zcHmwBpNyCO7xcdyZFUDFf1XwhIRs',
+    authDomain: 'todo-app-f2a04.firebaseapp.com',
+    messagingSenderId: '511326328594',
+  },
   production: false
 };
 
