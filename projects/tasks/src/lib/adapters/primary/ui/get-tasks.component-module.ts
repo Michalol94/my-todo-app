a4b196beb85aba0fccf186faf64c30690ac0ee66
Michalol94 <michal.olech94@gmail.com>
2022-04-05 22:31:28 +0200

    Ngx modal working

--- projects/tasks/src/lib/adapters/primary/ui/get-tasks.component-module.ts
+++ projects/tasks/src/lib/adapters/primary/ui/get-tasks.component-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { GetTasksComponent } from './get-tasks.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
-	imports: [CommonModule, ReactiveFormsModule, FormsModule, BsDropdownModule.forRoot()],
+	imports: [CommonModule, ReactiveFormsModule, FormsModule, BsDropdownModule.forRoot(), ModalModule.forRoot()],
 	declarations: [GetTasksComponent],
 	providers: [],
 	exports: [GetTasksComponent]
--- projects/tasks/src/lib/adapters/primary/ui/get-tasks.component.ts
+++ projects/tasks/src/lib/adapters/primary/ui/get-tasks.component.ts
@@ -1,21 +1,29 @@
-import { Component, ViewEncapsulation, ChangeDetectionStrategy, Inject } from '@angular/core';
+import { Component, ViewEncapsulation, ChangeDetectionStrategy, Inject, TemplateRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TaskDTO } from '../../../application/ports/secondary/task.dto';
 import { GETS_ALL_TASK_DTO, GetsAllTaskDtoPort } from '../../../application/ports/secondary/gets-all-task.dto-port';
 import { REMOVES_TASK_DTO, RemovesTaskDtoPort } from '../../../application/ports/secondary/removes-task.dto-port';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({ selector: 'lib-get-tasks', templateUrl: './get-tasks.component.html', encapsulation: ViewEncapsulation.None, changeDetection: ChangeDetectionStrategy.OnPush })
 export class GetTasksComponent {
 
 
 
+  modalRef?: BsModalRef;
   tasks$: Observable<TaskDTO[]> = this._getsAllTaskDto.getAll();
 
-  constructor(@Inject(GETS_ALL_TASK_DTO) private _getsAllTaskDto: GetsAllTaskDtoPort, @Inject(REMOVES_TASK_DTO) private _removesTaskDto: RemovesTaskDtoPort) {
+  constructor(private modalService: BsModalService, @Inject(GETS_ALL_TASK_DTO) private _getsAllTaskDto: GetsAllTaskDtoPort, @Inject(REMOVES_TASK_DTO) private _removesTaskDto: RemovesTaskDtoPort) {
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
   onItemClicked(id: string): void {
     this._removesTaskDto.remove(id);
+    this.modalRef?.hide()
   }
+
 
 }
